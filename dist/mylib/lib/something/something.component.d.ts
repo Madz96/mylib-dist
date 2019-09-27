c8a6de3f8d0d17b0783980ed170886c896aaa811
Madz96 <mad.subasinghe@gmail.com>
2019-09-27 10:48:49 +0530

    bleh

--- dist/mylib/lib/something/something.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { OnInit } from "@angular/core";
-export declare class SomethingComponent implements OnInit {
-    input: any;
-    constructor();
-    ngOnInit(): void;
-}
--- dist/mylib/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/mylib.service';
-export * from './lib/mylib.component';
-export * from './lib/mylib.module';
